56a997cd758da15086a2b18d4c7dfed59f2deb56
David Hain <dhain@zognot.org>
2009-10-30 20:15:07 -0700

    fix bug in slice2limit

--- orm/util.py
+++ orm/util.py
@@ -3,14 +3,18 @@ def slice2limit(slc):
         raise TypeError('step argument in slice is not supported')
     if slc.stop is None and slc.start is None:
         return
-    if (slc.stop is not None and slc.stop < 0) or (slc.start is not None
-                                                   and slc.start < 0):
+    if (
+        slc.stop is not None and slc.stop < 0) or (
+        slc.start is not None and slc.start < 0
+    ):
         raise NotImplementedError('negative slice values not yet supported')
     if slc.start is None:
         return 'limit %d' % (slc.stop,)
     limit = 'limit %d' % (slc.start,)
     if slc.stop is None:
         limit += ', -1'
+    elif slc.stop < slc.start:
+        raise ValueError('stop must be greater than start')
     else:
         limit += ', %d' % (slc.stop - slc.start,)
     return limit
